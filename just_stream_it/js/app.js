80a029f8b23e295f7e032d96dc45b675481f98bf
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-04-23 15:11:11 +0200

    feat(app.js): make api calls to gathers data for 2 genres + render 2 grids according to data fetched

--- just_stream_it/js/app.js
+++ just_stream_it/js/app.js
@@ -1,20 +1,37 @@
 import { fetchTitles, fetchTitleById, buildTitlesParams } from "./fetch.js";
-import { renderBestMovie, renderTopRated } from "./render.js";
+import { renderBestMovie, renderMovieGrid, renderRandomMovieGrid } from "./render.js";
+import {randomGenreOne, randomGenreTwo} from "./const.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    // ── Fetch top 7 films sorted by IMDB score ──────────────────────────────
+    // ── Fetch films' data from API ──────────────────────────────
+    // ── top 7 films sorted by IMDB score ──────────────────────────────
     const { data: titlesData, error: titlesError } = await fetchTitles(
         buildTitlesParams({ page: 1, page_size: 7, sort_by: "-imdb_score" })
     );
     if (titlesError) throw titlesError;
 
     const [bestMovie, ...topRatedMovies] = titlesData.results;
-    // ── Fetch full details of the best movie (description requires the /id endpoint) ──
+
+    // ── full details of the best movie ──
     const { data: bestMovieData, error: bestMovieError } = await fetchTitleById(bestMovie.id);
     if (bestMovieError) throw bestMovieError;
 
+    // ── 6 films sorted by a random genre ──────────────────────────────
+    const { data: moviesByRandomGenreOne, error: moviesByRandomGenreError } = await fetchTitles(
+        buildTitlesParams({ page: 1, page_size: 6, genre: randomGenreOne })
+    );
+    if (moviesByRandomGenreError) throw moviesByRandomGenreError;
+
+    // ── 6 films sorted by another random genre ──────────────────────────────
+    const { data: moviesByRandomGenreTwo, error: moviesByRandomGenreTwoError } = await fetchTitles(
+        buildTitlesParams({ page: 1, page_size: 6, genre: randomGenreTwo })
+    );
+    if (moviesByRandomGenreTwoError) throw moviesByRandomGenreTwoError;
+
     // ── Render ───────────────────────────────────────────────────────────────
     renderBestMovie(bestMovieData);
-    renderTopRated(topRatedMovies);
+    renderMovieGrid("#top-rated .grid", topRatedMovies);
+    renderRandomMovieGrid("random-category-one", randomGenreOne, moviesByRandomGenreOne.results);
+    renderRandomMovieGrid("random-category-two", randomGenreTwo, moviesByRandomGenreTwo.results);
 });

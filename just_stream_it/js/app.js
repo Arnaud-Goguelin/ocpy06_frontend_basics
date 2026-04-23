a7de0bcd4079556eb2e8a59e2076f07a713d65f1
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-04-23 15:31:53 +0200

    feat(app.js): call renderCategorySelect with remaining genres

--- just_stream_it/js/app.js
+++ just_stream_it/js/app.js
@@ -1,20 +1,10 @@
 import { fetchAllGenreNames, fetchTitles, fetchTitleById, buildTitlesParams } from "./fetch.js";
-import { renderBestMovie, renderMovieGrid, renderRandomMovieGrid } from "./render.js";
+import { renderBestMovie, renderMovieGrid, renderRandomMovieGrid,renderCategorySelect } from "./render.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
     // ── Fetch films' data from API ──────────────────────────────
-    // ── all genres ─────────────────────────────────────────────────────
-    const { data: allGenres, error: genresError } = await fetchAllGenreNames();
-    if (genresError) throw genresError;
 
-    const indexOne = Math.floor(Math.random() * allGenres.length);
-    const randomGenreOne = allGenres[indexOne];
-    allGenres.splice(indexOne, 1);
-
-    const indexTwo = Math.floor(Math.random() * allGenres.length);
-    const randomGenreTwo = allGenres[indexTwo];
-    allGenres.splice(indexTwo, 1);
     // ── top 7 films sorted by IMDB score ──────────────────────────────
     const { data: titlesData, error: titlesError } = await fetchTitles(
         buildTitlesParams({ page: 1, page_size: 7, sort_by: "-imdb_score" })
@@ -27,13 +17,25 @@ document.addEventListener("DOMContentLoaded", async () => {
     const { data: bestMovieData, error: bestMovieError } = await fetchTitleById(bestMovie.id);
     if (bestMovieError) throw bestMovieError;
 
-    // ── 6 films sorted by a random genre ──────────────────────────────
+    // ── all genres ─────────────────────────────────────────────────────
+    const { data: allGenres, error: genresError } = await fetchAllGenreNames();
+    if (genresError) throw genresError;
+
+    const indexOne = Math.floor(Math.random() * allGenres.length);
+    const randomGenreOne = allGenres[indexOne];
+    allGenres.splice(indexOne, 1);
+
+    const indexTwo = Math.floor(Math.random() * allGenres.length);
+    const randomGenreTwo = allGenres[indexTwo];
+    allGenres.splice(indexTwo, 1);
+
+    // ── 6 films sorted by random genre one ──────────────────────────────
     const { data: moviesByRandomGenreOne, error: moviesByRandomGenreError } = await fetchTitles(
         buildTitlesParams({ page: 1, page_size: 6, genre: randomGenreOne })
     );
     if (moviesByRandomGenreError) throw moviesByRandomGenreError;
 
-    // ── 6 films sorted by another random genre ──────────────────────────────
+    // ── 6 films sorted by random genre two ──────────────────────────────
     const { data: moviesByRandomGenreTwo, error: moviesByRandomGenreTwoError } = await fetchTitles(
         buildTitlesParams({ page: 1, page_size: 6, genre: randomGenreTwo })
     );
@@ -44,4 +46,5 @@ document.addEventListener("DOMContentLoaded", async () => {
     renderMovieGrid("#top-rated .grid", topRatedMovies);
     renderRandomMovieGrid("random-category-one", randomGenreOne, moviesByRandomGenreOne.results);
     renderRandomMovieGrid("random-category-two", randomGenreTwo, moviesByRandomGenreTwo.results);
+    renderCategorySelect(allGenres);
 });
